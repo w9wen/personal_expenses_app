3b9232c63dc37801ca30a43738deb692521d62d9
w9wen <william.wen@yes-energy.com.tw>
2022-03-21 09:04:34 +0800

    lamp($APP) Widgets & Configuring Widgets.

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,8 +13,11 @@ class CartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(
-          child: Text("\$${spendingAmount?.toStringAsFixed(0)}"),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text("\$${spendingAmount?.toStringAsFixed(0)}"),
+          ),
         ),
         SizedBox(
           height: 4,
